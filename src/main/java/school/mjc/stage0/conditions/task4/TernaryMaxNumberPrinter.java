7d42df73e89c3acbe6f79ce344028dc94e1a02eb
AliAmet <ametov1ali@gmail.com>
2023-10-24 16:05:16 +0600

    First commit

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result;
+        result = first > second? (first > third? first : third) : (second > third? second : third);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        TernaryMaxNumberPrinter ternaryMaxNumberPrinter = new TernaryMaxNumberPrinter();
+        ternaryMaxNumberPrinter.printGreatest(10, 12, 15);
     }
 }
